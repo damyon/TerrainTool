98a95acfc2451a47892a0be4e36c730f6821508e
Damyon Wiese <damyon@moodle.com>
2014-03-19 22:08:27 +0800

    Clean up licenses

--- src/SelectionRing.h
+++ src/SelectionRing.h
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2014  <copyright holder> <email>
+    Terrain Tool - Interactive terrain editor.
+    Copyright (C) 2014  Damyon Wiese <damyon.wiese@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- src/TerrainGenerator.cpp
+++ src/TerrainGenerator.cpp
@@ -1,6 +1,6 @@
 /*
-    <one line to give the program's name and a brief idea of what it does.>
-    Copyright (C) 2014  <copyright holder> <email>
+    Terrain Tool - Interactive terrain editor.
+    Copyright (C) 2014  Damyon Wiese <damyon.wiese@gmail.com>
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -15,7 +15,6 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 
 #include "TerrainGenerator.h"
 #include "DiamondSquareNoise.h"
